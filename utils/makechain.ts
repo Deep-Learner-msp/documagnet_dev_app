586cde1f29af726a0ca6134009e1c7ec9f3339aa
deep-learner-msp <mspraj00@gmail.com>
2023-07-25 17:00:02 +0530

    changedprompt

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -1,30 +1,36 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { RetrievalQAChain } from 'langchain/chains';
+import { PromptTemplate } from "langchain/prompts";
+
 
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to give an answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-
+Try to answer general questions as well. and if you get any source links makesure you give them as a clickable markdown links but only provide it for all State Street related questions, for the rest dont provide a markdown link like all generalised ones. and while providing the link please dont hallucinate, only provide factual links that you obtained from the answer.
 {context}
 
-Question: {query}
+Question: {question}
 Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new ChatOpenAI({
-    temperature: 0.9,
-    azureOpenAIApiKey: "api-key",
+    temperature: 0,
+    azureOpenAIApiKey: "",
     azureOpenAIApiInstanceName: "openai-ss",
     azureOpenAIApiDeploymentName: "ss-gpt-32k",
     azureOpenAIApiVersion: "2023-03-15-preview",
   });
   const chain = RetrievalQAChain.fromLLM(
     model,
+    
+    
     vectorstore.asRetriever(),
+
     {
+      prompt: PromptTemplate.fromTemplate(QA_PROMPT),
+
+    
       returnSourceDocuments: true,
-    }
+    },
   );
   return chain;
 };
